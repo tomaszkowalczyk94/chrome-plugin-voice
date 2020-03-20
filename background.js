4cdddecd6c7e326ba3ab1cd698da3e684e189a8c
test <test123>
2020-03-20 21:51:14 +0100

    set language

--- background.js
+++ background.js
@@ -8,6 +8,22 @@ chrome.commands.onCommand.addListener(function (command) {
 
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
+        //
+        // chrome.tts.getVoices(
+        //     function(voices) {
+        //         for (var i = 0; i < voices.length; i++) {
+        //             console.log('Voice ' + i + ':');
+        //             console.log('  name: ' + voices[i].voiceName);
+        //             console.log('  lang: ' + voices[i].lang);
+        //             console.log('  extension id: ' + voices[i].extensionId);
+        //             console.log('  event types: ' + voices[i].eventTypes);
+        //         }
+        //     });
+
         console.log(request);
-        chrome.tts.speak(request.text, {'lang': 'en-UK', 'rate': 1.0});
+        chrome.tts.speak(request.text, {
+            'lang': 'en-UK',
+            'voiceName' : 'Google UK English Male',
+            'rate': 0.8
+        });
     });
